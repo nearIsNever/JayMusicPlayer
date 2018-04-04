35b340d5fa138028fb859dcfe752de567d1d775f
nearIsNever <390276342@qq.com>
2018-04-04 15:03:27 +0800

    Signed-off-by: nearIsNever <390276342@qq.com>

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main(int argc, char *argv[])
     QTextCodec::setCodecForTr(QTextCodec::codecForName("utf-8"));
     JayMainWidget w;
     w.show();
-
+ //it is just a test
     return a.exec();
 }
